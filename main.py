3dc80295dd11943e4e76bd02ffe50283bf70ea05
keir stannard <keir@Keirs_PC.(none)>
2017-12-21 18:10:39 +0000

    added another test for checking int

--- main.py
+++ main.py
@@ -11,10 +11,13 @@ def hello():
 	tree = ET.parse('mini-schema.xml')
 	root = tree.getroot()
 
+	test_server.runTest()
+
 	for i in root:
 		for j in i:
 			name = j.find('name').text
 			value = int(j.find('value').text)
+			test_server.test_return(value)
 			notes = j.find('notes').text
 			print(name, value, notes)
 	return "test xml devices"
--- test_server.py
+++ test_server.py
@@ -1,9 +1,19 @@
 import socket;
+import unittest;
 
 def runTest():
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	result = sock.connect_ex(('localhost', 5000))
-	if result == 0:
-	   print "Port is open"
+
+	print("==> RUNNING WEBSERVER TEST")
+	if (result == 0):
+	   print ("==> Port is open\n")
 	else:
-	   print "Port is not open"
+	   print ("==> Port is not open\n")
+
+
+def test_return(x):
+	if(isinstance(x, int) == True):
+		return "valid"
+	else:
+		return "invalid"
